db10e310f92dcf6aa41d862bb770e9118d81ed80
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-09 21:04:54 +0800

    added a output and I removed the comments

--- six_digit_number_formatter.py
+++ six_digit_number_formatter.py
@@ -1,5 +1,3 @@
-# Ask a user to input a number between 0 and 1000
 number = input("Enter a number (0-1000):")
-# Convert to string and add zeros at the beginning to make it 6 digits
 formatted_number = number.zfill(6)
-# Display the output
+print("Number in 6-digit format:", formatted_number)
