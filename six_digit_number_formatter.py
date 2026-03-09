9f15b30b354a2d721383dd7ca2efc6aa082fb651
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-09 21:01:16 +0800

    added a number to fill 6 digits

--- six_digit_number_formatter.py
+++ six_digit_number_formatter.py
@@ -1,4 +1,5 @@
 # Ask a user to input a number between 0 and 1000
 number = input("Enter a number (0-1000):")
 # Convert to string and add zeros at the beginning to make it 6 digits
+formatted_number = number.zfill(6)
 # Display the output
